fd4074160a943fb8841780211395a58ff5719dc1
avi <hi@avi.im>
2015-11-20 20:42:59 +0530

    Add `settings.py`

---
+++ settings.py
@@ -0,0 +1,8 @@
+import os
+
+user_agent = 'when is my cakeday by /u/avinassh'
+scopes = ['identity']
+app_key = os.environ['APP_KEY']
+app_secret = os.environ['APP_SECRET']
+refresh_token = os.environ['REFRESH_TOKEN']
+access_token = os.environ['ACCESS_TOKEN']
